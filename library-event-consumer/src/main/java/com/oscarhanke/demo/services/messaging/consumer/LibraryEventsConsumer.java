98f8b832686c4b1c213b0cd74964a952f085d8fa
Oscar Hanke <steamoha@wp.pl>
2024-12-16 10:46:04 +0100

    implemented additional listener with manual message acknowledgement

--- library-event-consumer/src/main/java/com/oscarhanke/demo/services/messaging/consumer/LibraryEventsConsumer.java
+++ library-event-consumer/src/main/java/com/oscarhanke/demo/services/messaging/consumer/LibraryEventsConsumer.java
@@ -3,10 +3,12 @@ package com.oscarhanke.demo.services.messaging.consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@Profile("local")
 @Component
 @RequiredArgsConstructor
 public class LibraryEventsConsumer {
